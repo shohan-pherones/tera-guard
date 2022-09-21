7d2edc25f88f73e83a3e3324abf7ccd1cfca9de2
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-09-21 23:06:45 +0600

    modal added

--- script.js
+++ script.js
@@ -30,3 +30,107 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+// Selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector(".header");
+const allSections = document.querySelectorAll(".section");
+console.log(allSections);
+
+document.getElementById("section--1");
+const allButtons = document.getElementsByTagName("button");
+console.log(allButtons);
+
+console.log(document.getElementsByClassName("btn"));
+
+// Creating and inserting elements
+// .insertAdjacentHTML
+
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+// message.textContent = "We use cookies for improve functionality.";
+message.innerHTML =
+  'We use cookies for improving functionalities and analytics. <button class="btn btn--close-cookie">Got it</button>';
+// header.prepend(message); // Add into the first child
+header.append(message); // Add into the last child, exists only one place
+// header.append(message.cloneNode(true));
+// header.before(message);
+// header.after(message);
+
+// Delete elements
+document.querySelector(".btn--close-cookie").addEventListener("click", () => {
+  message.remove();
+  // message.parentElement.removeChild(message);
+});
+
+// Styles
+message.style.backgroundColor = "#37383d";
+message.style.width = "100vw";
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseInt(getComputedStyle(message).height) + 50 + "px";
+
+document.documentElement.style.setProperty("--color-primary", "orangered");
+
+// Attributes
+const logo = document.querySelector(".nav__logo");
+console.log(logo.alt);
+console.log(logo.className);
+console.log(logo.getAttribute("alt"));
+logo.alt = "Beautiful logo";
+logo.setAttribute("company", "Tera Guard");
+console.log(logo.src); // absolute
+console.log(logo.getAttribute("src")); // relative
+
+// Data attribute
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add("c");
+logo.classList.remove("c");
+logo.classList.toggle("c");
+logo.classList.contains("c");
+
+// Don't use
+logo.className = "shohan";
+
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+btnScrollTo.addEventListener("click", (e) => {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+  console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
+  console.log(
+    "h/w",
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  //Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: "smooth",
+  // });
+
+  section1.scrollIntoView({ behavior: "smooth" });
+});
