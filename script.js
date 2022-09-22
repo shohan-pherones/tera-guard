c7f404980895ef9c745a4be63ff1f1ab763f02b8
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-09-22 14:44:48 +0600

    tabbeb added

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ const message = document.createElement("div");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
 const navLinks = document.querySelector(".nav__links");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
 
 /////////////////////////////////////////////////////////////
 // Modal window
@@ -76,3 +79,28 @@ message.style.backgroundColor = "#37383d";
 message.style.width = "100vw";
 message.style.height =
   Number.parseInt(getComputedStyle(message).height) + 50 + "px";
+
+/////////////////////////////////////////////////////////////
+// Tabbed component
+/////////////////////////////////////////////////////////////
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach((tab) => tab.classList.remove("operations__tab--active"));
+  tabsContent.forEach((tabContent) =>
+    tabContent.classList.remove("operations__content--active")
+  );
+
+  // Activate tab
+  clicked.classList.add("operations__tab--active");
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
