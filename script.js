f81daeb23655b02f77b492a267480a38d75246cc
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-09-22 16:13:27 +0600

    intersector obs added

--- script.js
+++ script.js
@@ -85,6 +85,26 @@ nav.addEventListener("mouseover", handleHover.bind(0.5));
 nav.addEventListener("mouseout", handleHover.bind(1));
 
 /////////////////////////////////////////////////////////////
+// Sticky navigation
+/////////////////////////////////////////////////////////////
+
+const navHeight = nav.getBoundingClientRect().height;
+
+function stickyNav(entries) {
+  const entry = entries[0];
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+}
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
+
+/////////////////////////////////////////////////////////////
 // Cookie element
 /////////////////////////////////////////////////////////////
 
