5c953d2654e3c8fa17aaa2a73f253f1ec5f3dbd5
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-09-23 10:35:20 +0600

    bug

--- script.js
+++ script.js
@@ -155,7 +155,7 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 
 allSections.forEach((section) => {
   sectionObserver.observe(section);
-  // section.classList.add("section--hidden");
+  section.classList.add("section--hidden");
 });
 
 /////////////////////////////////////////////////////////////
